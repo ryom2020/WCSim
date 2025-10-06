eceeb567bcdaecf76e97a424a6a941c511a04e9b
ryom2020 <6220703@ed.tus.ac.jp>
2025-10-06 14:49:52 +0900

    IWCD geometry change

--- src/Build.cc
+++ src/Build.cc
@@ -5,5 +5,6 @@ const std::string HKFD_OD_WLS_PLATE_TYPE = "Kuraray";  // Definition
 //const std::string HKFD_OD_WLS_PLATE_TYPE = "Inr";  // Definition
 
 // define type for IWCD WLS plate
-const std::string IWCD_OD_WLS_PLATE_TYPE = "EljenEJ286";  // Definition
+//const std::string IWCD_OD_WLS_PLATE_TYPE = "EljenEJ286";  // Definition
 //const std::string IWCD_OD_WLS_PLATE_TYPE = "Kuraray";  // Definition
+const std::string IWCD_OD_WLS_PLATE_TYPE = "Inr";  // Definition
